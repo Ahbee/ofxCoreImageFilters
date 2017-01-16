194299496d656637db0c583d8a57fd8ef821c536
Abhi <Ahbee0@gmail.com>
2017-01-15 23:59:59 -0500

    with renderer

--- example_simple/src/main.mm
+++ example_simple/src/main.mm
@@ -4,11 +4,10 @@
 //========================================================================
 int main( ){
   
-  ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+    ofGLWindowSettings settings;
+    settings.setGLVersion(3,2); /// < select your GL Version here
+    ofCreateWindow(settings); ///< create your window here
+    ofRunApp(new ofApp());
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
 
 }
